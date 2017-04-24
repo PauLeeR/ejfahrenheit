8e585f111d7d91fc51d9e6a27565705143bcff4f
Paulina Rojas <lindaliel@gmail.com>
2017-04-24 16:38:41 -0300

    fahrenheit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//var celsius = 0;
+
+function convertirCelsius(far){
+	var far = document.getElementById("fahrenheit").value;//var que llama al valor fahrenheit q está en html
+        
+    alert ("El valor de los grados Celsius a Fahrenheit es" + ((far*(9/5)) + 32));//el resultado me lo muestra en un alert porque no sé como lo puede mostrar en otro botón
+ };
+	
+
